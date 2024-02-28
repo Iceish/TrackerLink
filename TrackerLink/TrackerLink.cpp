7950723ab8b51c2810fd5a3f4baff1ed67e198af
Romain <romain.murat@hesias.fr>
2024-02-28 22:27:34 +0100

    fix: support special characters

--- TrackerLink/TrackerLink.cpp
+++ TrackerLink/TrackerLink.cpp
@@ -33,6 +33,32 @@ void TrackerLink::RenderSettings()
 {
 	ImGui::TextUnformatted("Noting to see here.");
 }
+
+std::string urlencode(const std::string& s)
+{
+	static const char lookup[] = "0123456789abcdef";
+	std::stringstream e;
+	for (int i = 0, ix = s.length(); i < ix; i++)
+	{
+		const char& c = s[i];
+		if ((48 <= c && c <= 57) ||//0-9
+			(65 <= c && c <= 90) ||//abc...xyz
+			(97 <= c && c <= 122) || //ABC...XYZ
+			(c == '-' || c == '_' || c == '.' || c == '~')
+			)
+		{
+			e << c;
+		}
+		else
+		{
+			e << '%';
+			e << lookup[(c & 0xF0) >> 4];
+			e << lookup[(c & 0x0F)];
+		}
+	}
+	return e.str();
+}
+
 void TrackerLink::onTick(std::string eventName)
 {
 
@@ -64,7 +90,7 @@ void TrackerLink::onTick(std::string eventName)
 			
 			LOG("" + priw.GetPlayerName().ToString() + "/" + query + " " + PlatformMap.at(plateform));
 
-			string s = std::format("https://rocketleague.tracker.network/rocket-league/profile/{0}/{1}", PlatformMap.at(plateform), query);
+			string s = std::format("https://rocketleague.tracker.network/rocket-league/profile/{0}/{1}", PlatformMap.at(plateform), urlencode(query));
 
 			std::wstring stemp = std::wstring(s.begin(), s.end());
 			LPCWSTR url = stemp.c_str();
